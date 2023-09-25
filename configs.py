7bf844e85937a9efe8e25e669eb806837f1c2fe5
adampnggwa <punggawaadam@gmail.com>
2023-09-25 11:54:55 +0700

    add gitignore

--- configs.py
+++ configs.py
@@ -1,10 +1,14 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Config(BaseSettings):
-    api_key_openai: str = Field("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")
-    redirect_uri_register: str = Field("https://4706-149-108-13-248.ngrok-free.app/auth2callbackRegister")
-    redirect_uri_login: str = Field("https://4706-149-108-13-248.ngrok-free.app/auth2callbackLogin")
-    database_connections: str = Field("mysql://root:@127.0.0.1/ai_image")
-    
+    api_key_openai: str = Field(os.environ.get("API_KEY_OPENAI", "default_value"))
+    redirect_uri_register: str = Field(os.environ.get("REDIRECT_URI_REGISTER", "default_value"))
+    redirect_uri_login: str = Field(os.environ.get("REDIRECT_URI_LOGIN", "default_value"))
+    database_connections: str = Field(os.environ.get("DATABASE_CONNECTIONS", "default_value"))
+
 config = Config()
