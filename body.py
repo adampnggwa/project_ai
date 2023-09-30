101a93e44aaf6bd9808f6aab1a2ac2bee2e0c9b7
adampnggwa <punggawaadam@gmail.com>
2023-09-30 15:52:57 +0700

    fix error

--- body.py
+++ body.py
@@ -4,9 +4,3 @@ class ImageRequest(BaseModel):
     prompt: str
     size: str = "1024x1024"
     token: str
-
-class ImageEditRequest(BaseModel):
-    image: HttpUrl  # URL gambar yang akan diedit
-    mask: HttpUrl   # URL masker untuk gambar
-    prompt: str
-    token: str
