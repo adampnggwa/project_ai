9a607e15debf3a8bb326eaf6444f1ef764027419
adampnggwa <punggawaadam@gmail.com>
2023-10-05 18:27:07 +0700

    fix body.py

--- body.py
+++ body.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel
 
 class ImageRequest(BaseModel):
     prompt: str
     size: str = "1024x1024"
-    token: str
+    token: str  
