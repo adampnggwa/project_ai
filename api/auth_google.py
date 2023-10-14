a8bd9fcb4733d31d88db99538f49f58d12facd12
adampnggwa <punggawaadam@gmail.com>
2023-10-14 17:01:46 +0700

    fix error auth google

--- api/auth_google.py
+++ api/auth_google.py
@@ -6,8 +6,12 @@ from helping.auth import credentials_to_dict, create_token
 from helping.response import user_response
 from database.model import User
 from configs import config
+import os
 
 router = APIRouter(prefix='/google-auth', tags=['GOOGLE-AUTH'])
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
 
 @router.get('/signup')
 async def signupGoogle():
@@ -35,38 +39,10 @@ async def signinGoogle():
     )
     return RedirectResponse(authorization_url)
 
-@router.get('/auth2callbackSignin')
-async def callbackSignin(request: Request, state: str):
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        'client_secret.json',
-        scopes=['email', 'profile'],  
-        state=state
-    )
-    flow.redirect_uri = config.redirect_uri_login
-    authorization_response = str(request.url)
-    flow.fetch_token(authorization_response=authorization_response)
-    credentials = flow.credentials
-    creds = credentials_to_dict(credentials)
-    access_token = credentials.token
-
-    userinfo_endpoint = 'https://www.googleapis.com/oauth2/v3/userinfo'
-    user_info_response = requests.get(userinfo_endpoint, headers={'Authorization': f'Bearer {access_token}'})
-    user_info = user_info_response.json()
-    email = user_info.get("email")
-
-    existing_user = await User.filter(email=email).first()
-    if not existing_user:
-        raise HTTPException(status_code=400, detail="Invalid")
-    else:
-        user = await User.filter(email=email).first()
-        await create_token(user)
-        response = user_response(user)
-        return JSONResponse(response, status_code=200)
-    
 @router.get('/auth2callbackSignup')
 async def callbackSignup(request: Request, state: str):
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        'client_secret.json',
+        'credentials_google.json',
         scopes=['email', 'profile'],  
         state=state
     )
@@ -92,3 +68,31 @@ async def callbackSignup(request: Request, state: str):
         return JSONResponse(response, status_code=201)
     else:
         raise HTTPException(status_code=400, detail="Invalid")
+
+@router.get('/auth2callbackSignin')
+async def callbackSignin(request: Request, state: str):
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        'credentials_google.json',
+        scopes=['email', 'profile'],  
+        state=state
+    )
+    flow.redirect_uri = config.redirect_uri_login
+    authorization_response = str(request.url)
+    flow.fetch_token(authorization_response=authorization_response)
+    credentials = flow.credentials
+    creds = credentials_to_dict(credentials)
+    access_token = credentials.token
+
+    userinfo_endpoint = 'https://www.googleapis.com/oauth2/v3/userinfo'
+    user_info_response = requests.get(userinfo_endpoint, headers={'Authorization': f'Bearer {access_token}'})
+    user_info = user_info_response.json()
+    email = user_info.get("email")
+
+    existing_user = await User.filter(email=email).first()
+    if not existing_user:
+        raise HTTPException(status_code=400, detail="Invalid")
+    else:
+        user = await User.filter(email=email).first()
+        await create_token(user)
+        response = user_response(user)
+        return JSONResponse(response, status_code=200)
